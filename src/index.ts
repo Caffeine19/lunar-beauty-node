b6c4aa4b01cf5bf6c9074a56c7a4c3dfe70d508f
Caffeine19 <939597201@qq.com>
2022-10-08 23:09:16 +0800

    连接到mysql内部的数据库，实现migrate和seed

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-import { Prisma, PrismaClient } from '@prisma/client'
-import express from 'express'
+import { Prisma, PrismaClient } from "@prisma/client";
+import express from "express";
 
-const prisma = new PrismaClient()
-const app = express()
+const prisma = new PrismaClient();
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.post(`/signup`, async (req, res) => {
-  const { name, email, posts } = req.body
+  const { name, email, posts } = req.body;
 
   const postData = posts?.map((post: Prisma.PostCreateInput) => {
-    return { title: post?.title, content: post?.content }
-  })
+    return { title: post?.title, content: post?.content };
+  });
 
   const result = await prisma.user.create({
     data: {
@@ -21,24 +21,24 @@ app.post(`/signup`, async (req, res) => {
         create: postData,
       },
     },
-  })
-  res.json(result)
-})
+  });
+  res.json(result);
+});
 
 app.post(`/post`, async (req, res) => {
-  const { title, content, authorEmail } = req.body
+  const { title, content, authorEmail } = req.body;
   const result = await prisma.post.create({
     data: {
       title,
       content,
       author: { connect: { email: authorEmail } },
     },
-  })
-  res.json(result)
-})
+  });
+  res.json(result);
+});
 
-app.put('/post/:id/views', async (req, res) => {
-  const { id } = req.params
+app.put("/post/:id/views", async (req, res) => {
+  const { id } = req.params;
 
   try {
     const post = await prisma.post.update({
@@ -48,16 +48,16 @@ app.put('/post/:id/views', async (req, res) => {
           increment: 1,
         },
       },
-    })
+    });
 
-    res.json(post)
+    res.json(post);
   } catch (error) {
-    res.json({ error: `Post with ID ${id} does not exist in the database` })
+    res.json({ error: `Post with ID ${id} does not exist in the database` });
   }
-})
+});
 
-app.put('/publish/:id', async (req, res) => {
-  const { id } = req.params
+app.put("/publish/:id", async (req, res) => {
+  const { id } = req.params;
 
   try {
     const postData = await prisma.post.findUnique({
@@ -65,35 +65,35 @@ app.put('/publish/:id', async (req, res) => {
       select: {
         published: true,
       },
-    })
+    });
 
     const updatedPost = await prisma.post.update({
       where: { id: Number(id) || undefined },
       data: { published: !postData?.published },
-    })
-    res.json(updatedPost)
+    });
+    res.json(updatedPost);
   } catch (error) {
-    res.json({ error: `Post with ID ${id} does not exist in the database` })
+    res.json({ error: `Post with ID ${id} does not exist in the database` });
   }
-})
+});
 
 app.delete(`/post/:id`, async (req, res) => {
-  const { id } = req.params
+  const { id } = req.params;
   const post = await prisma.post.delete({
     where: {
       id: Number(id),
     },
-  })
-  res.json(post)
-})
+  });
+  res.json(post);
+});
 
-app.get('/users', async (req, res) => {
-  const users = await prisma.user.findMany()
-  res.json(users)
-})
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+});
 
-app.get('/user/:id/drafts', async (req, res) => {
-  const { id } = req.params
+app.get("/user/:id/drafts", async (req, res) => {
+  const { id } = req.params;
 
   const drafts = await prisma.user
     .findUnique({
@@ -103,22 +103,22 @@ app.get('/user/:id/drafts', async (req, res) => {
     })
     .posts({
       where: { published: false },
-    })
+    });
 
-  res.json(drafts)
-})
+  res.json(drafts);
+});
 
 app.get(`/post/:id`, async (req, res) => {
-  const { id }: { id?: string } = req.params
+  const { id }: { id?: string } = req.params;
 
   const post = await prisma.post.findUnique({
     where: { id: Number(id) },
-  })
-  res.json(post)
-})
+  });
+  res.json(post);
+});
 
-app.get('/feed', async (req, res) => {
-  const { searchString, skip, take, orderBy } = req.query
+app.get("/feed", async (req, res) => {
+  const { searchString, skip, take, orderBy } = req.query;
 
   const or: Prisma.PostWhereInput = searchString
     ? {
@@ -127,7 +127,7 @@ app.get('/feed', async (req, res) => {
           { content: { contains: searchString as string } },
         ],
       }
-    : {}
+    : {};
 
   const posts = await prisma.post.findMany({
     where: {
@@ -140,13 +140,13 @@ app.get('/feed', async (req, res) => {
     orderBy: {
       updatedAt: orderBy as Prisma.SortOrder,
     },
-  })
+  });
 
-  res.json(posts)
-})
+  res.json(posts);
+});
 
-const server = app.listen(3000, () =>
+const server = app.listen(3008, () =>
   console.log(`
-🚀 Server ready at: http://localhost:3000
-⭐️ See sample requests: http://pris.ly/e/ts/rest-express#3-using-the-rest-api`),
-)
+🚀 Server ready at: http://localhost:3008
+⭐️ See sample requests: http://pris.ly/e/ts/rest-express#3-using-the-rest-api`)
+);
