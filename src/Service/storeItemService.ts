9c5a2dfebdd384ea2f10153192f9df5014d89ada
Caffeine19 <108726581+Caffeine19@users.noreply.github.com>
2023-01-28 10:03:15 +0800

    添加calculatePreservationStatus

--- src/Service/storeItemService.ts
+++ src/Service/storeItemService.ts
@@ -1,12 +1,36 @@
 import prisma from "./prisma";
 
-import { ApplyingTime } from "@prisma/client";
+import { ApplyingTime, StoreItem } from "@prisma/client";
 
 import fs from "fs";
+import dayjs from "dayjs";
+
+type WithPreservationStatus<T> = T & {
+  isOpened: boolean;
+  isExpired: boolean;
+};
+
+const calculatePreservationStatus = (
+  storeItem: StoreItem
+): WithPreservationStatus<StoreItem> => {
+  const isOpened = storeItem.openedTime ? true : false;
+
+  const safeTime = dayjs(storeItem.productionTime).add(
+    storeItem.shelfTime,
+    "month"
+  );
+  const isExpired = safeTime.unix() > dayjs().unix() ? false : true;
+
+  return {
+    ...storeItem,
+    isOpened,
+    isExpired,
+  };
+};
 
 export const findByUser = async (userId: number) => {
   try {
-    const storeItemList = await prisma.storeItem.findMany({
+    const storeItems = await prisma.storeItem.findMany({
       where: {
         userId,
         deleted: false,
@@ -15,23 +39,26 @@ export const findByUser = async (userId: number) => {
         product: true,
       },
     });
+
     const basePath = "../lunar-beauty-node/src/static/productImages/Product/";
     const taskList: any[] = [];
-
-    storeItemList.forEach((p) => {
+    storeItems.forEach((p) => {
       taskList.push(
         fs.promises.readFile(basePath + p.product.images, {
           encoding: "base64",
         })
       );
     });
-
     const productImageList = await Promise.all(taskList);
-
-    storeItemList.forEach((p, index) => {
-      p.product.images = productImageList[index];
+    storeItems.forEach((storeItem, index) => {
+      storeItem.product.images = productImageList[index];
     });
-    return storeItemList;
+
+    const storeItemWithPreservationStatus = storeItems.map((storeItem) => {
+      return calculatePreservationStatus(storeItem);
+    });
+
+    return storeItemWithPreservationStatus;
   } catch (error) {
     throw error;
   }
@@ -74,7 +101,11 @@ export const updateById = async (
         isRunout,
       },
     });
-    return updatedStoreItem;
+
+    const updatedStoreItemWithPreservationStatus =
+      calculatePreservationStatus(updatedStoreItem);
+
+    return updatedStoreItemWithPreservationStatus;
   } catch (error) {
     throw error;
   }
