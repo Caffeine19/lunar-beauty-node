53b34b7680c5a81b543ef9a7ea64729087a97159
Caffeine19 <108726581+Caffeine19@users.noreply.github.com>
2023-01-28 10:02:48 +0800

    添加dayjs

--- prisma/migrations/20230124030516_/migration.sql
+++ prisma/migrations/20230124030516_/migration.sql
@@ -8,7 +8,7 @@
 */
 -- AlterTable
 ALTER TABLE `comment` DROP COLUMN `rank`,
-    ADD COLUMN `mark` DOUBLE NOT NULL;
+    ADD COLUMN `mark` DOUBLE NOT NUL L;
 
 -- AlterTable
 ALTER TABLE `product` DROP COLUMN `mark`;
